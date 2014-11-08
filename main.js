5f88a3017b5ed0c3d6fac82211bb7dae5513c8e7
Paul Wang <pauwang@ebay.com>
2014-11-08 04:54:58 -0500

    adding jin's version of main.js

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ var camera, scene, renderer, projector, light;
 var objects = [], objectsControls = [], cameraControls;
 var coords1, coords2, coords3;
 var lastControlsIndex = -1, controlsIndex = -1, index = -1;
+var planetsToExplode = [];
+//////////////settings/////////
+var dirs = [];
+var movementSpeed = 80;
+var totalObjects = 1000;
+var objectSize = 10;
+var sizeRandomness = 4000;
+var colors = [0xFF0FFF, 0xCCFF00, 0xFF000F, 0x996600, 0xFFFFFF];
+/////////////////////////////////
 
 function init() {
   // is webgl supported?
@@ -13,11 +22,14 @@ function init() {
   // renderer
   renderer = new THREE.WebGLRenderer({antialias: true});
   renderer.setSize($(window).width(), $(window).height());
+  renderer.setClearColor(0xffffff, 1);
   $("#container").append(renderer.domElement);
 
   // camera
-  camera = new THREE.PerspectiveCamera(50, $(window).width()/$(window).height(), 0.1, 10000);
-
+  camera = new THREE.PerspectiveCamera(25, $(window).width()/$(window).height(), 0.1, 10000);
+  camera.position.x = 500;
+  camera.position.y = 500;
+  camera.position.z = 500;
   var origin = new THREE.Vector3(0, 0, 0);
   camera.lookAt(origin);
 
@@ -28,14 +40,14 @@ function init() {
   cameraControls.rotateSpeed    = 3;
   cameraControls.rotateHands    = 1;
   cameraControls.rotateFingers  = [2, 3];
-
+  
   cameraControls.zoomEnabled    = true;
   cameraControls.zoomSpeed      = 6;
   cameraControls.zoomHands      = 1;
   cameraControls.zoomFingers    = [4, 5];
   cameraControls.zoomMin        = 50;
   cameraControls.zoomMax        = 2000;
-
+  
   cameraControls.panEnabled     = true;
   cameraControls.panSpeed       = 2;
   cameraControls.panHands       = 2;
@@ -43,21 +55,10 @@ function init() {
   cameraControls.panRightHanded = false; // for left-handed person
 
   // world
-  scene = new THREE.Scene();
-
-
-	// light
-	light = new THREE.PointLight(0xefefef);
-	light.position = camera.position;
-	scene.add(light);
-
-  // light
-  light = new THREE.PointLight(0xefefef);
-  light.position = camera.position;
-  scene.add(light);
+  scene = new THREE.Scene(); 
 
   // projector
-  projector = new THREE.Projector();
+  projector = new THREE.Projector();       
 
   // camera target coordinate system
   coords1 = new THREE.ArrowHelper(new THREE.Vector3(1, 0, 0), origin, 75, 0xcccccc);
@@ -72,19 +73,56 @@ function init() {
   var vertArray = lineGeometry.vertices;
   vertArray.push(new THREE.Vector3(1000, 0, 0), origin, new THREE.Vector3(0, 1000, 0), origin, new THREE.Vector3(0, 0, 1000));
   lineGeometry.computeLineDistances();
-  var lineMaterial = new THREE.LineDashedMaterial({color: 0xffffffff, dashSize: 1, gapSize: 2});
+  var lineMaterial = new THREE.LineDashedMaterial({color: 0xcccccc, dashSize: 1, gapSize: 2});
   var coords = new THREE.Line(lineGeometry, lineMaterial);
   scene.add(coords);
 
-	// add star field
-	addStars();
-	
-	// add planets
-	addPlanets(20);
+  // spheres
+  for (var i = 0; i < 20; i ++) {
+    var geometry = new THREE.SphereGeometry(Math.random()*60, 128, 128);
+    var object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({color: 0xefefef}));
+    object.position.x = Math.random()* 300 - 150;
+    object.position.y = Math.random()* 300 - 150;
+    object.position.z = Math.random()* 200 - 100;
+
+    object.rotation.x = Math.random()*2*Math.PI;
+    object.rotation.y = Math.random()*2*Math.PI;
+    object.rotation.z = Math.random()*2*Math.PI;
+
+    object.receiveShadow = true;
+
+    // leap object controls
+    var objectControls = new THREE.LeapObjectControls(camera, object);
+
+    objectControls.rotateEnabled  = true;
+    objectControls.rotateSpeed    = 3;
+    objectControls.rotateHands    = 1;
+    objectControls.rotateFingers  = [2, 3];
+    
+    objectControls.scaleEnabled   = true;
+    objectControls.scaleSpeed     = 3;
+    objectControls.scaleHands     = 1;
+    objectControls.scaleFingers   = [4, 5];
+    
+    objectControls.panEnabled     = true;
+    objectControls.panSpeed       = 3;
+    objectControls.panHands       = 2;
+    objectControls.panFingers     = [6, 12];
+    objectControls.panRightHanded = false; // for left-handed person
+
+    scene.add(object);
+    objects.push(object);
+    objectsControls.push(objectControls);
+  };
+
+  // light
+  light = new THREE.PointLight(0xefefef);
+  light.position = camera.position;
+  scene.add(light);
 
   // listen to resize event
   window.addEventListener('resize', onWindowResize, false);
-
+  window.addEventListener('keypress', detonate, false );
 
   // render (if no leap motion controller is detected, then this call is needed in order to see the plot)
   render();
@@ -100,7 +138,7 @@ function changeControlsIndex() {
         if (index > -1) objects[index].material.color.setHex(0xff0000);
       }
     };
-  };
+  }; 
   lastControlsIndex = controlsIndex;
 };
 
@@ -149,7 +187,7 @@ function focusObject(frame) {
     projector.unprojectVector(vector, camera);
     var raycaster = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
     var intersects = raycaster.intersectObjects(objects);
-    if (intersects.length > 0) {
+    if (intersects.length > 0) { 
       var i = 0;
       while(!intersects[i].object.visible) i++;
       var intersected = intersects[i];
@@ -162,135 +200,64 @@ function focusObject(frame) {
   return -2;
 };
 
-function addStars() {
-	var FAR = 10000;
-	var color = new THREE.Color(1, 1, 1);
-	var geometry, material, particleSystem;
-	var x, y, z;
-	
-	for(var i=0; i<10; i++) {
-		geometry = new THREE.Geometry();
-		material = new THREE.ParticleCanvasMaterial({
-			color:0xFFFFFF,
-			size:2,
-			map: THREE.ImageUtils.loadTexture("assets/img/particle.png"),
-			blending: THREE.AdditiveBlending,
-		  transparent: true
-		});
-		
-		for(var j=0; j<1000; j++) {
-      x = (Math.random() - .5 ) * FAR/15;
-      y = (Math.random() - .5 ) * FAR/15;
-      z = (Math.random() - .5 ) * FAR/15;
-      geometry.vertices.push( new THREE.Vector3( x , y , z ) );
-		}
-		
-		particleSystem = new THREE.ParticleSystem(geometry, material);
-
-		scene.add(particleSystem);
-	}	
-}
-
-function getPlanetMaterialParams(planetName) {
-	var params = {};
-	switch(planetName) {
-		case 'earth':
-			params['map'] = THREE.ImageUtils.loadTexture("assets/img/planets/earthmap.jpg");
-			params['bumpMap'] = THREE.ImageUtils.loadTexture("assets/img/planets/earthmap.jpg");
-			params['bumpScale'] = 0.05;
-			break;
-		case 'jupiter':
-			params['map'] = THREE.ImageUtils.loadTexture("assets/img/planets/jupitermap.jpg");
-			params['bumpMap'] = THREE.ImageUtils.loadTexture("assets/img/planets/jupitermap.jpg");
-			params['bumpScale'] = 0.00;
-			break;
-		case 'mars':
-			params['map'] = THREE.ImageUtils.loadTexture("assets/img/planets/marsmap1k.jpg");
-			params['bumpMap'] = THREE.ImageUtils.loadTexture("assets/img/planets/marsmap1k.jpg");
-			params['bumpScale'] = 0.02;
-			break;
-		case 'mercury':
-			params['map'] = THREE.ImageUtils.loadTexture("assets/img/planets/mercurymap.jpg");
-			params['bumpMap'] = THREE.ImageUtils.loadTexture("assets/img/planets/mercurybump.jpg");
-			params['bumpScale'] = 0.00;
-			break;
-		case 'neptune':
-			params['map'] = THREE.ImageUtils.loadTexture("assets/img/planets/neptunemap.jpg");
-			params['bumpMap'] = THREE.ImageUtils.loadTexture("assets/img/planets/neptunemap.jpg");
-			params['bumpScale'] = 0.02;
-			break;
-		case 'pluto':
-			params['map'] = THREE.ImageUtils.loadTexture("assets/img/planets/plutomap1k.jpg");
-			params['bumpMap'] = THREE.ImageUtils.loadTexture("assets/img/planets/plutobump1k.jpg");
-			params['bumpScale'] = 0.00;
-			break;
-		case 'saturn':
-			params['map'] = THREE.ImageUtils.loadTexture("assets/img/planets/saturnmap.jpg");
-			params['bumpMap'] = THREE.ImageUtils.loadTexture("assets/img/planets/saturnmap.jpg");
-			params['bumpScale'] = 0.00;
-			break;
-		case 'uranus':
-			params['map'] = THREE.ImageUtils.loadTexture("assets/img/planets/uranusmap.jpg");
-			params['bumpMap'] = THREE.ImageUtils.loadTexture("assets/img/planets/uranusmap.jpg");
-			params['bumpScale'] = 0.00;
-			break;
-		case 'venus':
-			params['map'] = THREE.ImageUtils.loadTexture("assets/img/planets/venusmap.jpg");
-			params['bumpMap'] = THREE.ImageUtils.loadTexture("assets/img/planets/venusbump.jpg");
-			params['bumpScale'] = 0.02;
-			break;
-	}
-	
-	return params;
-};
-
-
-function getRandomPlanetName(){
-	var planetNames = ['earth', 'jupiter', 'mars', 
-										 'mercury', 'neptune', 'pluto', 
-										 'saturn', 'uranus', 'venus'];
-	var randNum = Math.floor(Math.random() * planetNames.length);
-	return planetNames[randNum];
-}
-
-function addPlanet(planetName) {
-	var materialsParams = getPlanetMaterialParams(planetName);
-	var geometry = new THREE.SphereGeometry(0.5, 32, 32);
-	var material = new THREE.MeshPhongMaterial(materialsParams);
-	var sphere = new THREE.Mesh(geometry, material);
-	scene.add(sphere);
-}
-
-function addPlanets(n) {
-	var randPlanetName, materialParams, geometry, material, sphere;
-
-	for(var i=0; i < n; i++ ) {
-		randPlanetName = getRandomPlanetName();
-		materialParams = getPlanetMaterialParams(randPlanetName);
-		
-		geometry = new THREE.SphereGeometry(Math.floor(Math.random()*20) + 16, 32, 32);
-		material = new THREE.MeshPhongMaterial(materialParams);
-		
-		sphere = new THREE.Mesh(geometry, material);
-    sphere.position.x = Math.floor(Math.random() * 300) - (50 * i);
-    sphere.position.y = Math.floor(Math.random() * 300) - (50 * i);
-    sphere.position.z = Math.floor(Math.random() * 400) - (50 * i);
-		
-		sphere.receiveShadow = true;
-		
-		scene.add(sphere);
-	}
-}
-
-function render() {
+function render() {   
   renderer.render(scene, camera);
 };
+
+function ExplodeAnimation(meshPlanet)
+{
+  var geometry = new THREE.Geometry();
+  
+  for (i = 0; i < totalObjects; i ++) 
+  { 
+    var vertex = new THREE.Vector3();
+    vertex.x = meshPlanet.position.x;
+    vertex.y = meshPlanet.position.y;
+    vertex.z = meshPlanet.position.z;
+  
+    geometry.vertices.push( vertex );
+    dirs.push({x:(Math.random() * movementSpeed)-(movementSpeed/2),y:(Math.random() * movementSpeed)-(movementSpeed/2),z:(Math.random() * movementSpeed)-(movementSpeed/2)});
+  }
+  var material = new THREE.ParticleBasicMaterial( { size: objectSize,  color: colors[Math.round(Math.random() * colors.length)] });
+  var particles = new THREE.ParticleSystem( geometry, material );
+  
+  this.object = particles;
+  this.status = true;
+  
+  this.xDir = (Math.random() * movementSpeed)-(movementSpeed/2);
+  this.yDir = (Math.random() * movementSpeed)-(movementSpeed/2);
+  this.zDir = (Math.random() * movementSpeed)-(movementSpeed/2);
+  
+  scene.add( this.object  ); 
+  
+  this.update = function(){
+    if (this.status == true){
+      var pCount = totalObjects;
+      while(pCount--) {
+        var particle =  this.object.geometry.vertices[pCount]
+        particle.y += dirs[pCount].y;
+        particle.x += dirs[pCount].x;
+        particle.z += dirs[pCount].z;
+      }
+      this.object.geometry.verticesNeedUpdate = true;
+    }
+  }
+  
+};
+
 
 function onWindowResize() {
   camera.aspect = $(window).width()/$(window).height();
   camera.updateProjectionMatrix();
   renderer.setSize($(window).width(), $(window).height());
   render();
+};
+
+function detonate(){
+  var pCount = planetsToExplode.length;
+    while(pCount--) {
+      planetsToExplode[pCount].update();
+    };
 };
 
 $(function(){
@@ -306,6 +273,7 @@ $(function(){
     if (index == -1) {
       cameraControls.update(frame);
     } else {
+      planetsToExplode.push(new ExplodeAnimation(objectsControls[index]));
       objectsControls[index].update(frame);
     };
 
